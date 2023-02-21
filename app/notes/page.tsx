d59c24b3acdec8d1407b1a390d8181a1c3866fcb
Firstname Lastname <enzoesper7@gmail.com>
2023-02-21 03:43:14 -0300

    adding some styles to the note component and fetching the notes from the api (this way is treated like a static page)

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import Link from "next/link";
+import styles from "./Notes.module.css";
+
+async function getNotes() {
+  const res = await fetch("http://127.0.0.1:8090/api/collections/notes/records?page=1&per_page=10");
+  const data = await res.json();
+
+  return data?.items as any[];
+}
 
 export default async function NotesPage() {
+  const notes = await getNotes();
+
   return (
     <div>
       <h1>Notes</h1>
+      {notes.map((note) => (
+        <Note key={note.id} note={note} />
+      ))}
     </div>
+  );
+}
+
+function Note({ note }: any) {
+  const { id, title, content, created } = note || {};
+
+  return (
+    <Link href={`/notes/${id}`}>
+      <div className={styles.note}>
+        <h2>{title}</h2>
+        <h5>{content}</h5>
+        <p>{created}</p>
+      </div>
+    </Link>
   );
 }
