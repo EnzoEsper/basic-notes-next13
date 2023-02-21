775f8a2f1a689e7c436c57778ecc75cee1358148
Firstname Lastname <enzoesper7@gmail.com>
2023-02-21 03:59:38 -0300

    using the orm provided by pocketbase to fetch the data

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link";
 import styles from "./Notes.module.css";
+import PocketBase from "pocketbase";
+
+// this is the way to customize the caching behavior of the page for the case when using the orm
+export const dynamic = "auto",
+  dynamicParams = true,
+  revalidate = 0,
+  fetchCache = "auto",
+  runtime = "nodejs",
+  preferenceRegion = "auto";
 
 async function getNotes() {
-  const res = await fetch("http://127.0.0.1:8090/api/collections/notes/records?page=1&per_page=10", {
-    cache: "no-store",
-  });
-  const data = await res.json();
+  // this way we fetch data using pocketbase orm and we have to customize the cache exporting certain variables from the page (the dynamic const exported above)
+  const pb = new PocketBase("http://127.0.0.1:8090");
+  const data = await pb.collection("notes").getList(1, 10);
+
+  // this way we fetch data using fetch and we customize the cache with the cache option
+  // const res = await fetch("http://127.0.0.1:8090/api/collections/notes/records?page=1&per_page=10", {
+  //   cache: "no-store",
+  // });
+  // const data = await res.json();
 
   return data?.items as any[];
 }
